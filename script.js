7529866ca5a3a00762c68a65a3cd11a4dd9ff8ab
Michael Hilt <michael_hilt@comcast.com>
2022-07-27 12:26:25 -0400

    added validation to check that all forms are filled

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 window.addEventListener("load", function() {
-   let form = this.document.querySelector("form");
+
+   let pilotNode = document.querySelector("input[name=pilotName]");
+   let copilotNode = document.querySelector("input[name=copilotName]");
+   let fuelLevelNode = document.querySelector("input[name=fuelLevel]");
+   let cargoMassNode = document.querySelector("input[name=cargoMass]");
+
+   let form = document.querySelector("form");
+
    form.addEventListener("submit", function(event) {
-      event.preventDefault();
-      alert("Do not leave any field(s) empty.");
+
+      if (pilotNode.value === "" || copilotNode.value === "" || fuelLevelNode.value === "" || cargoMassNode.value === ""){
+         alert("Do not leave any field(s) empty.");
+         event.preventDefault();
+      };
+      
    });
-   //form.addEventListener("submit", function() {
-   //})
+
 })
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
