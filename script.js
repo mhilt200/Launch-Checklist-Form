2a0e3ddc285c1a0d689e9dc75947a9ffb77fc236
Michael Hilt <michael_hilt@comcast.com>
2022-07-28 14:45:13 -0400

    finished 'updating shuttle requirements' (still would like 'launchStatusNode' not to display in certain instances)

--- script.js
+++ script.js
@@ -20,41 +20,46 @@ window.addEventListener("load", function() {
 
       if (pilotInput.value === "" || copilotInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === ""){
          alert("Do not leave any field(s) empty.");
-      };
+         event.preventDefault();
+      } 
       
       if ((!isNaN(pilotInput.value)) || (!isNaN(copilotInput.value))){
          alert("Both pilots names must only contain letters.");
-      }
-
+         event.preventDefault();
+      } 
+      
       if (isNaN(fuelLevelInput.value) || isNaN(cargoMassInput.value)){
          alert("Both Fuel Level and Cargo Mass must only contain numbers.");
-      }
-      
-      itemStatusNode.style.visibility = 'visible';
-      pilotStatusNode.innerHTML = `Pilot: ${pilotInput.value}`;
-      copilotStatusNode.innerHTML = `Co-Pilot: ${copilotInput.value}`;
-      
-      if (fuelLevelInput.value < 10000) {
-         fuelStatusNode.innerHTML = `Not enough fuel for journey! Must be more than 10,000 liters.`;
-         launchStatusNode.innerHTML = `Shuttle not ready for launch`;
-         launchStatusNode.style.color = "red"
+         event.preventDefault();
       } else {
-         fuelStatusNode.innerHTML = `Fuel level check passed`;
+      
+         if (fuelLevelInput.value < 10000) {
+            itemStatusNode.style.visibility = 'visible';
+            fuelStatusNode.innerHTML = `Not enough fuel for journey! Must be more than 10,000 liters.`;
+            launchStatusNode.innerHTML = `Shuttle not ready for launch`;
+            launchStatusNode.style.color = "red"
+            event.preventDefault();
+         } 
+
+         if (cargoMassInput.value > 10000) {
+            itemStatusNode.style.visibility = 'visible';
+            cargoStatusNode.innerHTML = `Too much mass for takeoff! Must be less than 10,000 kilograms.`;
+            launchStatusNode.innerHTML = `Shuttle not ready for launch`;
+            launchStatusNode.style.color = "red"
+            event.preventDefault();
+         } 
+         
+         if (fuelLevelInput.value >= 10000 && cargoMassInput.value <= 10000) {
+            itemStatusNode.style.visibility = 'visible';
+            launchStatusNode.innerHTML = `Shuttle is ready for launch`;
+            launchStatusNode.style.color = "green";
+            pilotStatusNode.innerHTML = `Pilot: ${pilotInput.value}`;
+            copilotStatusNode.innerHTML = `Co-Pilot: ${copilotInput.value}`;
+            fuelStatusNode.innerHTML = `Fuel level check passed`;
+            cargoStatusNode.innerHTML = `Cargo mass check passed`;
+         }
       }
-
-      if (cargoMassInput.value > 10000) {
-         cargoStatusNode.innerHTML = `Too much mass for takeoff! Must be less than 10,000 kilograms.`;
-         launchStatusNode.innerHTML = `Shuttle not ready for launch`;
-         launchStatusNode.style.color = "red"
-      } else {
-         cargoStatusNode.innerHTML = `Cargo mass check passed`;
-      }
-
-      launchStatusNode.innerHTML = `Shuttle is ready for launch`;
-      launchStatusNode.style.color = "green";
-
    });
-
 })
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
