2d672cf7caee1bdcba8f0ba80402f24cf72b0de1
Michael Hilt <michael_hilt@comcast.com>
2022-07-27 23:56:01 -0400

    almost completed 'updating shuttle requirements'

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ window.addEventListener("load", function() {
    let copilotInput = document.getElementById("copilotName");
    let fuelLevelInput = document.getElementById("fuelLevel");
    let cargoMassInput = document.getElementById("cargoMass");
+
    let itemStatusNode = document.getElementById("itemStatus");
+   let pilotStatusNode = document.getElementById("pilotStatus");
+   let copilotStatusNode = document.getElementById("copilotStatus");
+   let fuelStatusNode = document.getElementById("fuelStatus");
+   let launchStatusNode = document.getElementById("launchStatus");
+   let cargoStatusNode = document.getElementById("cargoStatus");
 
    let formNode = document.getElementById("launchForm");
 
@@ -22,9 +28,30 @@ window.addEventListener("load", function() {
 
       if (isNaN(fuelLevelInput.value) || isNaN(cargoMassInput.value)){
          alert("Both Fuel Level and Cargo Mass must only contain numbers.");
-      };
+      }
       
       itemStatusNode.style.visibility = 'visible';
+      pilotStatusNode.innerHTML = `Pilot: ${pilotInput.value}`;
+      copilotStatusNode.innerHTML = `Co-Pilot: ${copilotInput.value}`;
+      
+      if (fuelLevelInput.value < 10000) {
+         fuelStatusNode.innerHTML = `Not enough fuel for journey! Must be more than 10,000 liters.`;
+         launchStatusNode.innerHTML = `Shuttle not ready for launch`;
+         launchStatusNode.style.color = "red"
+      } else {
+         fuelStatusNode.innerHTML = `Fuel level check passed`;
+      }
+
+      if (cargoMassInput.value > 10000) {
+         cargoStatusNode.innerHTML = `Too much mass for takeoff! Must be less than 10,000 kilograms.`;
+         launchStatusNode.innerHTML = `Shuttle not ready for launch`;
+         launchStatusNode.style.color = "red"
+      } else {
+         cargoStatusNode.innerHTML = `Cargo mass check passed`;
+      }
+
+      launchStatusNode.innerHTML = `Shuttle is ready for launch`;
+      launchStatusNode.style.color = "green";
 
    });
 
